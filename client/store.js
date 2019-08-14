606cfd7993b0b271e7e4621fb5934da62cb17dd6
Beno Suriano <bjsuriano@gmail.com>
2019-08-13 21:25:09 -0600

    created store.

---
+++ client/store.js
@@ -0,0 +1,37 @@
+import { createStore } from 'redux'
+
+const ADD_GROCERY = 'ADD_GROCERY'
+
+const initialState = {
+  groceries: []
+}
+
+let nextId = 0
+
+export const addGrocery = (text) => (
+  {
+    type: ADD_GROCERY,
+    id: nextId++,
+    text
+  }
+)
+
+const reducer = (state = initialState, action) => {
+
+  switch(action.type) {
+    case ADD_GROCERY:
+      const newGrocery = {
+        id: action.id,
+        text: action.text,
+        bought: false
+      }
+
+      return { ...state, groceries: [...state.groceries, newGrocery] }
+    default:
+      return state
+  }
+}
+
+const store = createStore(reducer)
+
+export default store
