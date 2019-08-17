9693ad337addcc4ae6e87a4c092e5daea07d8c69
Beno Suriano <bjsuriano@gmail.com>
2019-08-17 09:57:39 -0600

    added quantities for items and can dispatch action to update them by id.

--- client/store.js
+++ client/store.js
@@ -13,6 +13,7 @@ let nextId = 0
 const ADD_GROCERY = 'ADD_GROCERY'
 const TOGGLE_GROCERY = 'TOGGLE_GROCERY'
 const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
+const SET_QUANTITY = 'SET_QUANTITY'
 
 const SHOW_ALL = 'SHOW_ALL'
 const SHOW_BOUGHT = 'SHOW_BOUGHT'
@@ -22,9 +23,11 @@ const SHOW_ACTIVE = 'SHOW_ACTIVE'
 export const addGrocery = text => ({type: ADD_GROCERY, id: nextId++, text})
 export const toggleGrocery = id => ({type: TOGGLE_GROCERY, id})
 export const setVisibilityFilter = value => ({type: SET_VISIBILITY_FILTER, value})
+export const setQuantity = (id, quant) => ({ type: SET_QUANTITY, id, quant})
 
 // reducer
 const reducer = (state = initialState, action) => {
+  let groceries
 
   switch(action.type) {
     case ADD_GROCERY:
@@ -32,13 +35,14 @@ const reducer = (state = initialState, action) => {
       const newGrocery = {
         id: action.id,
         text: action.text,
+        quantity: 1,
         bought: false
       }
 
       return { ...state, groceries: [...state.groceries, newGrocery] }
 
     case TOGGLE_GROCERY:
-      const groceries = state.groceries.map(grocery => {
+      groceries = state.groceries.map(grocery => {
         if(grocery.id === action.id) {
           return {...grocery, bought: !grocery.bought}
         } else {
@@ -50,6 +54,18 @@ const reducer = (state = initialState, action) => {
 
     case SET_VISIBILITY_FILTER:
       return {...state, visibilityFilter: action.value}
+
+    case SET_QUANTITY:
+      groceries = state.groceries.map(grocery => {
+        if(grocery.id === action.id) {
+          return {...grocery, quantity: action.quant}
+        } else {
+          return grocery
+        }
+      })
+
+      return {...state, groceries}
+
     default:
       return state
   }
@@ -58,10 +74,9 @@ const reducer = (state = initialState, action) => {
 const store = createStore(reducer, applyMiddleware(loggerMiddleware))
 
 // Manual test (temporary hard coded dispatches just for fun)
-// store.dispatch(addGrocery('milk'))
-// store.dispatch(addGrocery('cookies'))
-// store.dispatch(toggleGrocery(0))
-// store.dispatch(toggleGrocery(0))
-// store.dispatch(setVisibilityFilter(SHOW_ACTIVE))
+store.dispatch(addGrocery('milk'))
+store.dispatch(addGrocery('cookies'))
+store.dispatch(setQuantity(0,20))
+
 
 export default store
